bab539fe2af5afdc84835f7ae53b921ffb5bc47d
Xavier <xto@ffly4u.com>
2020-05-11 10:57:20 +0200

    Bugfix dimension of argmax interpolated

--- pymath/optimize.py
+++ pymath/optimize.py
@@ -2,9 +2,12 @@ import numpy as np
 
 
 def argmax_interpolated(arr):
-    argmax = np.argmax(arr, axis=0)
+    _arr = np.reshape(arr, (np.shape(arr)[0], -1))
+    argmax = np.argmax(_arr, axis=0)
+    if np.any(argmax == 0) or np.any(argmax == np.shape(_arr)[0] - 1):
+        raise ValueError("A maximum value is outside interpolation range")
     index = argmax[None] + np.arange(3)[:, None] - 1
-    index = index + (np.arange(arr.shape[1]) * arr.shape[0])[None]
-    y1, y2, y3 = arr.T.ravel()[index]
+    index = index + (np.arange(_arr.shape[1]) * _arr.shape[0])[None]
+    y1, y2, y3 = _arr.T.ravel()[index]
     frac = (y1 - y3) / (2 * (y3 - 2 * y2 + y1))
-    return argmax + frac
+    return np.reshape(argmax + frac, arr.shape[1:])
--- test/test_optimize.py
+++ test/test_optimize.py
@@ -17,3 +17,11 @@ class Test(TestCase):
         plt.plot(x.ravel(), y.T)
         err = np.abs((argmax - 1) - real_max.ravel())
         assert err.max() < 1e-10
+
+    def test_argmax_interpolated2(self):
+        a, b, c = [0.00021424366603001315, -0.006707034546251528, 0.13929253965589541]
+        x = np.arange(50)
+        y = a * x ** 2 + b * x + c
+        argmax = np.argmax_interpolated(-y)
+        err = np.abs(argmax + b / (2 * a))
+        assert err < 1e-10
